aee7bbbf31756ca488c7440f1d75f94db0a9697d
pisketti <tero.kadenius@gmail.com>
2016-03-20 16:00:46 +0200

    Removes client side routing as it is not needed

--- src/clj/game_of_life/handler.clj
+++ src/clj/game_of_life/handler.clj
@@ -7,10 +7,7 @@
 
 (def mount-target
   [:div#app
-      [:h3 "ClojureScript has not been compiled!"]
-      [:p "please run "
-       [:b "lein figwheel"]
-       " in order to start the compiler"]])
+      [:h3 "Loading..."]])
 
 (def loading-page
   (html5
@@ -26,8 +23,7 @@
 
 (defroutes routes
   (GET "/" [] loading-page)
-  (GET "/about" [] loading-page)
-  
+
   (resources "/")
   (not-found "Not Found"))
 
--- src/cljs/game_of_life/core.cljs
+++ src/cljs/game_of_life/core.cljs
@@ -1,8 +1,5 @@
 (ns game-of-life.core
     (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]
               [game-of-life.rules :as rules]
               [clojure.core.matrix :as matrix]))
 
@@ -32,16 +29,11 @@
          [0 0 0 0 0 0 0 0 0 0]
          [1 0 0 0 0 0 0 0 0 1]])
 
-
-
 (def empty-grid [[0 0 0]
                  [0 0 0]
                  [0 0 0]])
 
-(def test-grid [0 1 0 1])
-
 (def app-state (atom {:grid g1
-                      :test-grid test-grid
                       ;;:running? false
                       :timer nil
                       :settings {:cell-size 10
@@ -51,48 +43,41 @@
 
 (println "empty grid: " empty-grid)
 (println "current grid: " (get-in @app-state [:grid]))
-;;(println "next gen: " (rules/next-generation g))
 
-(defn test-canvas
-  "Creates animated clock based on seconds only."
+(defn game-of-life-canvas
+  "Creates a game of life board"
   []
   (let [comp-state (atom {:canvas nil
                           :ctx nil})]
     (reagent/create-class
-     {
+     {:display-name "game-of-life-canvas"
+
       :component-did-mount
       (fn [comp]
         (reset! comp-state {:ctx (.getContext (reagent/dom-node comp) "2d")}))
-      :display-name "game-of-life-grid"
-      :reagent-render (fn [comp]
-                        (let [ctx  (get-in @comp-state [:ctx])
-                              grid (get-in @app-state  [:grid])
-                              {:keys [cell-size
-                                      live-cell-color
-                                      dead-cell-color]} (get-in @app-state [:settings])]
-                          (if ctx
-                            (let [render-cell! (fn [[y x] item]
-                                                 (if (= item 1)
-                                                   (set! (.-fillStyle ctx) live-cell-color)
-                                                   (set! (.-fillStyle ctx) dead-cell-color))
-                                                 (let [slot-x (* cell-size x)
-                                                       slot-y (* cell-size y)]
-                                                   (.fillRect ctx slot-x slot-y cell-size cell-size)))]
-                              (matrix/emap-indexed render-cell! grid)))
 
-                          )
-                        (println "comp-state: " @comp-state)
-                        (println "app-state: " @app-state)
-                        [:canvas {:width 200 :height 200 :style {:border "1px solid black" }}])})))
+      :reagent-render
+      (fn []
+        (let [ctx  (get-in @comp-state [:ctx])
+              grid (get-in @app-state  [:grid])
+              {:keys [cell-size
+                      live-cell-color
+                      dead-cell-color]} (get-in @app-state [:settings])]
+          (if ctx
+            (let [render-cell! (fn [[y x] item]
+                                 (if (= item 1)
+                                   (set! (.-fillStyle ctx) live-cell-color)
+                                   (set! (.-fillStyle ctx) dead-cell-color))
+                                 (let [slot-x (* cell-size x)
+                                       slot-y (* cell-size y)]
+                                   (.fillRect ctx slot-x slot-y cell-size cell-size)))]
+              (matrix/emap-indexed render-cell! grid))))
+        (println "comp-state: " @comp-state)
+        (println "app-state: " @app-state)
+        [:canvas {:width 200 :height 200 :style {:border "1px solid black" }}])})))
 
 (defn start-game! []
-  (swap! app-state update-in [:running] (constantly true))
-  ;; (loop []
-  ;;   (if (get-in @app-state [:running])
-  ;;     (do
-  ;;       (js/setTimeout #(println "running") 1000)
-  ;;       (recur))))
-  (let [next-gen! (fn []
+    (let [next-gen! (fn []
                     (let [next-gen (rules/next-generation (:grid @app-state))]
                       (println "next gen: " next-gen)
                       (swap! app-state update-in [:grid] (constantly next-gen))))
@@ -100,37 +85,20 @@
     (swap! app-state update-in [:timer] (constantly create-timer))))
 
 (defn stop-game! []
-  ;; (swap! app-state update-in [:running] (constantly true))
-  ;; (loop []
-  ;;   (if (get-in @app-state [:running])
-  ;;     ))
-  (println "Stopping timer")
   (js/clearInterval (:timer @app-state))
-  (swap! app-state update-in [:timer] (constantly nil))
-  ;;(js/alert "stop")
-  )
+  (swap! app-state update-in [:timer] (constantly nil)))
 
 (defn reset-board! []
-  (println "Stopping timer")
   (stop-game!)
   (swap! app-state update-in [:grid] (constantly g1)))
 
 ;; -------------------------
-;; Views
+;; View
 
-(defn home-page []
-  [:div [:h2 "Welcome to game-of-life 2"]
-   [:div [:a {:href "/about"} "go to about page"]
-    ]])
-
-(defn about-page []
-  [:div [:h2 "About game-of-life"]
-   [:div [:a {:href "/"} "go to the home page"]]])
-
-(defn current-page []
+(defn game-of-life []
   [:div {:style {:margin "0px 0px 0px 50px"}}
    [:div [:h3 "Game of life"]
-    [test-canvas]]
+    [game-of-life-canvas]]
 
    (let [running? (not (nil? (:timer @app-state)))]
      [:div
@@ -142,31 +110,10 @@
                 :style {:width "100px" :margin "0px 0px 0px 1px"}} "PAUSE"]])
    [:div
     [:button {:on-click #(reset-board!)
-              :style {:width "200px" :margin "0px 1px 0px 0px"}} "reset"]
-    ]])
-
-;; -------------------------
-;; Routes
-
-(secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
-
-(secretary/defroute "/about" []
-  (session/put! :current-page #'about-page))
+              :style {:width "200px" :margin "0px 1px 0px 0px"}} "reset"]]])
 
 ;; -------------------------
 ;; Initialize app
 
-(defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
-
 (defn init! []
-  (accountant/configure-navigation!
-    {:nav-handler
-     (fn [path]
-       (secretary/dispatch! path))
-     :path-exists?
-     (fn [path]
-       (secretary/locate-route path))})
-  (accountant/dispatch-current!)
-  (mount-root))
+  (reagent/render [game-of-life] (.getElementById js/document "app")))
