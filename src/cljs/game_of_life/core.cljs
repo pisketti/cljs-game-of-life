c6a94ae8fd3549fd2ad4552b8e60d28ad587bab1
pisketti <tero.kadenius@gmail.com>
2016-03-30 22:32:25 +0300

    Adds known start patterns and previews them
    Also changes the look and feel of the buttons

--- src/cljs/game_of_life/core.cljs
+++ src/cljs/game_of_life/core.cljs
@@ -31,20 +31,43 @@
 
 (defonce empty-grid (rules/create-empty-grid 40 40))
 
+(def rows  40)
+(def columns 40 )
+
+
+;; Patterns
+
+(defonce glider (rules/expand-to [[0 1 0]
+                                  [0 0 1]
+                                  [1 1 1]] rows columns))
+
+(defonce lwss (rules/expand-to [[0 1 0 0 1]
+                                [1 0 0 0 0]
+                                [1 0 0 0 1]
+                                [1 1 1 1 0]] rows columns))
+
 (def app-state (atom {:grid empty-grid
+                      :preview nil
                       :timer nil
                       :settings {:cell-size 10
                                  :board-width 400
                                  :board-height 400
                                  :live-cell-color "red"
                                  :dead-cell-color "white"
+                                 :preview-color "#b3b3b3"
                                  :interval 500}}))
+
+(defn set-preview [preview-matrix]
+  (swap! app-state update-in [:preview] (constantly preview-matrix)))
+
+(defn set-pattern [pattern-matrix]
+  (swap! app-state update-in [:grid] (constantly pattern-matrix))
+  (set-preview nil))
 
 (defn game-of-life-board
   "Creates a game of life board"
   []
-  (let [
-        comp-state (atom {:canvas nil
+  (let [comp-state (atom {:canvas nil
                           :ctx nil
                           :mousedown false})
         get-canvas-coords (fn [[window-x window-y] canvas]
@@ -58,23 +81,37 @@
                                   [canvas-x canvas-y] (get-canvas-coords [window-x window-y] canvas)
                                   matrix-x (.floor js/Math (/ canvas-x 10))
                                   matrix-y (.floor js/Math (/ canvas-y 10))]
-                              (println "window coords: " [[window-x window-y]] " canvas coords: " [[canvas-x canvas-y]] " matrix coors: " [matrix-x matrix-y])
+                              (println "window coords: " [[window-x window-y]]
+                                       " canvas coords: " [[canvas-x canvas-y]]
+                                       " matrix coors: " [matrix-x matrix-y])
                               [matrix-x matrix-y]))
 
         render-cell! (fn [ctx {:keys [cell-size live-cell-color dead-cell-color]}]
+                       (println "live-cell-color: " live-cell-color)
                        (fn [[y x] item]
-                         (if (= item 1)
-                           (set! (.-fillStyle ctx) live-cell-color)
-                           (set! (.-fillStyle ctx) dead-cell-color))
-                         (let [slot-x (* cell-size x)
-                               slot-y (* cell-size y)]
-                           (.fillRect ctx slot-x slot-y cell-size cell-size))))
+                         (let [alive? (= item 1)
+                               redraw-dead? dead-cell-color]
+                           (if alive?
+                             (set! (.-fillStyle ctx) live-cell-color)
+                             (set! (.-fillStyle ctx) dead-cell-color))
+                           (let [slot-x (* cell-size x)
+                                 slot-y (* cell-size y)]
+                             (if (or alive? redraw-dead?)
+                               (.fillRect ctx slot-x slot-y cell-size cell-size))))))
+
         render-board! (fn []
                         (let [ctx  (get-in @comp-state [:ctx])
                               grid (get-in @app-state  [:grid])
-                              settings (get-in @app-state [:settings])]
-                          (if ctx
-                            (matrix/emap-indexed (render-cell! ctx settings) grid))))]
+                              preview (get-in @app-state  [:preview])
+                              settings (get-in @app-state [:settings])
+                              preview-color (:preview-color settings)
+                              preview-settings (-> settings
+                                                   (assoc :live-cell-color preview-color)
+                                                   (assoc :dead-cell-color nil))]
+                          (when ctx
+                            (matrix/emap-indexed (render-cell! ctx settings) grid)
+                            (when preview
+                              (matrix/emap-indexed (render-cell! ctx preview-settings) preview)))))]
     (r/create-class
      {:display-name "game-of-life-board"
 
@@ -112,7 +149,7 @@
         (render-board!)
         [:canvas {:width (get-in @app-state [:settings :board-width])
                   :height (get-in @app-state [:settings :board-height])
-                  :style {:border "1px solid black" }}])})))
+                  :style {:border "1px solid black" :cursor "default"}}])})))
 
 (defn start-game! []
     (let [next-gen! (fn []
@@ -133,21 +170,38 @@
 ;; View
 
 (defn game-of-life []
-  [:div {:style {:margin "0px 0px 0px 50px"}}
+  [:div {:style {:margin "0px 0px 0px 0px"}}
    [:div [:h3 "Game of life"]
     [game-of-life-board]]
 
    (let [running? (not (nil? (:timer @app-state)))]
      [:div
-      [:button {:on-click #(start-game!)
-                :disabled running?
-                :style {:width "200px" :margin "0px 1px 0px 0px"}} "PLAY"]
-      [:button {:on-click #(stop-game!)
-                :disabled (not running?)
-                :style {:width "200px" :margin "0px 0px 0px 1px"}} "PAUSE"]])
+      [:div {:class "half-size"}
+       [:button {:on-click #(start-game!)
+                      :disabled running?
+                      :class "full-btn green"} "PLAY"]]
+      [:div {:class "half-size"}
+       [:button {:on-click #(stop-game!)
+                 :disabled (not running?)
+                 :class "full-btn red"} "PAUSE"]]])
    [:div
     [:button {:on-click #(reset-board!)
-              :style {:width "400px" :margin "0px 1px 0px 0px"}} "reset"]]])
+              :class "full-btn dark-gray"} "CLEAR"]]
+
+   [:div
+    [:h5 "space ships"]
+
+    [:div
+     [:button {:on-click #(set-pattern glider)
+               :on-mouse-over #(set-preview glider)
+               :on-mouse-out #(set-preview nil)
+               :class "small-btn blue"} "Glider"]
+     [:button {:on-click #(set-pattern lwss)
+               :on-mouse-over #(set-preview lwss)
+               :on-mouse-out #(set-preview nil)
+               :class "small-btn blue"} "LW space ship"]
+     ]
+    ]])
 
 ;; -------------------------
 ;; Initialize app
